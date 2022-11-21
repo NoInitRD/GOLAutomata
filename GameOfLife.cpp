ed526c69166028c3767b4294f0a40035e6ac8ad6
NoInitRD <noinitrd@gmail.com>
2022-11-21 14:44:04 -0500

    small fixes

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -1,4 +1,5 @@
 /*
+ *
  * GameOfLife.cpp
  *
  *  Created on: Nov 18, 2022
@@ -17,6 +18,7 @@ using namespace std;
 class Arena {
 public:
         vector<vector<bool> > _vector;
+	int _generation;
 
         void flipCellStatus(uint x, uint y);
         bool liveOrDie(uint x, uint y);
@@ -26,13 +28,13 @@ public:
         void generateRandom(void);
 
         Arena(){};
-        Arena(int const size) : _vector(size, vector<bool>(size, false)) {}
-        Arena(Arena const &src) {_vector = src._vector;}
+        Arena(int const size): _vector(size, vector<bool>(size, false)) { _generation = 1; }
+        Arena(Arena const &src) { _vector = src._vector; }
 };
 
 bool Arena::cellExists(uint x, uint y) { return x < _vector.size() && y < _vector.size(); }
 
-void clearScreen(void) { cout << "\033[2J\033[1;1H"; }
+void clearScreen(void) { cout << "\033[2J\033[1;1H"; } //ANSI escape characters
 
 void Arena::flipCellStatus(uint x, uint y)
 {
@@ -56,12 +58,12 @@ bool Arena::liveOrDie(uint x, uint y)
 		if(getCellStatus(x - 1, y + i)) numAlive++;
 
 	//check above and below cell
-        if(getCellStatus(x, y + 1)) numAlive++;
-        if(getCellStatus(x, y - 1)) numAlive++;
+	for(int j = 1; j >= -1; j -= 2)
+		if(getCellStatus(x, y + j)) numAlive++;
 
 	//check right side of cell
-	for(int j = 1; j > -2; j--) 
-		if(getCellStatus(x + 1, y + j)) numAlive++;
+	for(int k = 1; k > -2; k--) 
+		if(getCellStatus(x + 1, y + k)) numAlive++;
 
 	//determine if cell will live or die
         if(currentCellStatus && (numAlive == 2 || numAlive == 3)) return true; //survival and reproduction
@@ -84,16 +86,17 @@ void Arena::ShowAndUpdateCells(void)
                 }
 		cout << endl;
         }
-
         if(_vector == newVector) exit(1);
         _vector = newVector;
+	_generation++;
 
 }
 
 bool randomBool(void)
 {
-	srand((unsigned) chrono::system_clock::now().time_since_epoch().count());
-	return rand() % 2 == 1;
+	uint time = chrono::system_clock::now().time_since_epoch().count(); //get time in ms
+	srand(time); //seed random with time
+	return rand() % 2 == 1; //boolean conversion
 }
 
 void Arena::generateRandom(void)
@@ -111,17 +114,17 @@ void sleep(int ms) { this_thread::sleep_for(std::chrono::milliseconds(ms)); }
 
 int main()
 {
-        Arena *a = new Arena(30);
-        a->generateRandom();
-        a->ShowAndUpdateCells();
+        Arena *a = new Arena(60);
+
+	a->generateRandom(); //randomly fills arena with cells
 
 	int generation = 0;
 
         while(true)
         {
                 a->ShowAndUpdateCells();
-		generation++;
-		cout << generation << endl;
+		cout << "Generation: ";
+		cout << a->_generation << endl;
                 sleep(50);
 		clearScreen();
         }
