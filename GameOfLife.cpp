6cdbb5883190bd735049465783c8c8804a03245b
NoInitRD <noinitrd@gmail.com>
2022-11-21 11:51:11 -0500

    added random seeding, changed how cell neighbors are counted

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -9,12 +9,14 @@
 #include <vector>
 #include <thread>
 #include <stdlib.h>
+#include <random>
+#include <chrono>
 
 using namespace std;
 
 class Arena {
 public:
-        vector<vector<bool>> _vector;
+        vector<vector<bool> > _vector;
 
         void showCells(void);
         void flipCellStatus(uint x, uint y);
@@ -35,7 +37,7 @@ void clearScreen(void) { cout << "\033[2J\033[1;1H"; }
 
 void Arena::showCells(void)
 {
-        clearScreen();
+	//displays each cell in a grid using 2D vector
         for(uint y = 0; y < _vector.size(); y++)
         {
                 for(uint x = 0; x < _vector.size(); x++)
@@ -64,23 +66,28 @@ bool Arena::liveOrDie(uint x, uint y)
         short numAlive = 0;
         bool currentCellStatus = getCellStatus(x, y);
 
-        if(getCellStatus(x - 1, y + 1)) numAlive++;
-        if(getCellStatus(x - 1, y)) numAlive++;
-        if(getCellStatus(x - 1, y - 1)) numAlive++;
+	//check left side of cell
+	for(int i = 1; i > -2; i--) 
+		if(getCellStatus(x - 1, y + i)) numAlive++;
+
+	//check above and below cell
         if(getCellStatus(x, y + 1)) numAlive++;
         if(getCellStatus(x, y - 1)) numAlive++;
-        if(getCellStatus(x + 1, y + 1)) numAlive++;
-        if(getCellStatus(x + 1, y)) numAlive++;
-        if(getCellStatus(x + 1, y - 1)) numAlive++;
 
+	//check right side of cell
+	for(int j = 1; j > -2; j--) 
+		if(getCellStatus(x + 1, y + j)) numAlive++;
+
+	//determine if cell will live or die
         if(currentCellStatus && (numAlive == 2 || numAlive == 3)) return true; //survival and reproduction
         return !currentCellStatus && numAlive == 3; //death
 }
 
 void Arena::updateCells(void)
 {
-        vector<vector<bool>> newVector(_vector.size(), vector<bool>(_vector.size(), false));
+        vector<vector<bool> > newVector(_vector.size(), vector<bool>(_vector.size(), false));
 
+	//updates each cell in 2D vector
         for(uint y = 0; y < _vector.size(); y++)
         {
                 for(uint x = 0; x < _vector.size(); x++)
@@ -91,39 +98,47 @@ void Arena::updateCells(void)
         }
 
         if(_vector == newVector) exit(1);
-
         _vector = newVector;
+
+}
+
+bool randomBool(void)
+{
+	srand((unsigned) chrono::system_clock::now().time_since_epoch().count());
+	return rand() % 2 == 1;
 }
 
 void Arena::generateRandom(void)
 {
         for(uint y = 0; y < _vector.size(); y++)
-        {
+	{
                 for(uint x = 0; x < _vector.size(); x++)
-                {
-                        _vector[x][y] = rand() % 2;
-                }
-        }
-}
+		{
+                        _vector[x][y] = randomBool();
+		}
+	}
+}	
 
 void sleep(int ms) { this_thread::sleep_for(std::chrono::milliseconds(ms)); }
 
 int main()
 {
         Arena *a = new Arena(30);
-
         a->generateRandom();
-
         a->showCells();
+
+	int generation = 0;
 
         while(true)
         {
                 a->updateCells();
                 a->showCells();
-                sleep(10);
+		generation++;
+		cout << generation << endl;
+                sleep(50);
+		clearScreen();
         }
 
         delete a;
-
         return 0;
 }
